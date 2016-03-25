e41670d2b2b5441acf09c996458860d52b6851f0
Jaco Thiart <jaco.thiart.android@gmail.com>
2016-03-25 14:44:05 +0200

    django coffee ppackage manager build

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='django-coffee',
+      version='0.0.4',
+      description='Django Coffee',
+      url='https://github.com/jacothiart/django-coffee',
+      author='Jaco Thiart',
+      author_email='jaco@jacothiart.com',
+      license='MIT',
+      packages=['coffee'],
+      zip_safe=False)
